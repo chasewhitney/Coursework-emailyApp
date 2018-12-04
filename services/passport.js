491c53f1c6206b448824d7f33ae27837e9fbcc07
Chase <ChaseEWhitney@gmail.com>
2018-12-04 13:19:40 -0600

    google strategy refactor

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,13 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // User in db
-          done(null, existingUser);
-        } else {
-          // User not in db
-          new User({ googleId: profile.id }).save().then(user => {
-            done(null, user);
-          });
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
