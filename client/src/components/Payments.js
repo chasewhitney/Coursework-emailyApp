cfb84242274948bfd8263c15579be0fb648c150c
Chase <ChaseEWhitney@gmail.com>
2018-12-07 17:11:12 -0600

    created handleToken action and billingRoutes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <StripeCheckout
-      name="Emaily"
-      description="$5 for 5 email credits"
-      amount={500}
-      token={token => console.log('token:', token)}
-      stripeKey={process.env.REACT_APP_STRIPE_KEY}
-    />
-  );
-};
+import * as actions from '../actions';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(
+  mapStateToProps,
+  actions
+)(Payments);
