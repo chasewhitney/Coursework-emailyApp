691c565dfd4c0d980a480a1179654ce2c426648e
Chase <ChaseEWhitney@gmail.com>
2018-12-07 16:19:28 -0600

    added more info to Stripe popup component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,6 +4,8 @@ import StripeCheckout from 'react-stripe-checkout';
 export default () => {
   return (
     <StripeCheckout
+      name="Emaily"
+      description="$5 for 5 email credits"
       amount={500}
       token={token => console.log('token:', token)}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
