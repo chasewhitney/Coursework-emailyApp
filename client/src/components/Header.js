4aa17e4649c3f48f2ce7310c86cb78d30169d0a2
Chase <ChaseEWhitney@gmail.com>
2018-12-10 14:07:33 -0600

    billing functional

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import StripeCheckout from './Payments';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -18,9 +18,12 @@ class Header extends Component {
       default:
         return [
           <li key="1">
-            <StripeCheckout />
+            <Payments />
           </li>,
-          <li key="2">
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="3">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
