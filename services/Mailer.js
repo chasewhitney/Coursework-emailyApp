22f9237b32cd4dfe17aac64d0cd44343612bdf54
Chase <ChaseEWhitney@gmail.com>
2018-12-13 13:11:23 -0600

    @sendgrid sends emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,52 +1,22 @@
-const sendgrid = require('sendgrid');
-const helper = sendgrid.mail;
-const keys = '../config/keys';
+const sgMail = require('@sendgrid/mail');
+const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {
+class Mailer {
   constructor({ subject, recipients }, content) {
-    super();
-
-    this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email('no-reply@emaily.com');
-    this.subject = subject;
-    this.body = new helper.Content('text/html', content);
-    this.recipients = this.formatAddresses(recipients);
-
-    this.addContent(this.body);
-    this.addClickTracking();
-    this.addRecipients();
-  }
-
-  formatAddresses(recipients) {
-    return recipients.map(({ email }) => {
-      return new helper.Email(email);
-    });
-  }
-
-  addClickTracking() {
-    const trackingSettings = new helper.TrackSettings();
-    const clickTracking = new helper.ClickTracking(true, true);
-
-    trackSettings.setClickTracking(clickTracking);
-    this.addTrackSettings(trackingSettings);
-  }
-
-  addRecipients() {
-    const personalize = new helper.Personalization();
-    this.recipients.forEach(recipient => {
-      personalize.addTo(recipient);
-    });
-    this.addPersonalization(personalize);
+    this.message = {
+      to: recipients,
+      from: 'no-reply@emaily.com',
+      subject: subject,
+      html: content,
+      trackingSettings: {
+        clickTracking: { enable: true },
+      },
+    };
+    sgMail.setApiKey(keys.sendGridKey);
   }
 
   async send() {
-    const request = this.sgApi.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: this.toJSON(),
-    });
-
-    const response = await this.sgApi.API(request);
+    const response = await sgMail.sendMultiple(this.message);
     return response;
   }
 }
