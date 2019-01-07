95ff1c4f61c68dc4ba1464a84ca4942cf17c9150
Chase <ChaseEWhitney@gmail.com>
2019-01-07 11:53:51 -0600

    pulling unique email and surveyIds from webhook req

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
 const mongoose = require('mongoose');
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -12,7 +15,20 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqueBy(compactEvents, 'email', 'surveyId');
+
+    console.log('hit webhook route');
+    console.log(uniqueEvents);
     res.send({});
   });
 
