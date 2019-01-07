6a7dc3c0e58d80c46dc669d518e0aa80bc569fab
Chase <ChaseEWhitney@gmail.com>
2019-01-07 13:47:02 -0600

    updating survey data on vote submission

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,7 +17,7 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -25,11 +25,25 @@ module.exports = app => {
         }
       })
       .compact()
-      .uniqueBy('email', 'surveyId')
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+          }
+        ).exec();
+      })
       .value();
 
     console.log('hit webhook route');
-    console.log(events);
+
     res.send({});
   });
 
